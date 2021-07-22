403bee372ca1fa3ff9fa066931dfca1928396839
prashant dahal <prashantdah41@gmail.com>
2021-07-22 15:13:34 -0400

    Add store and remove biolerplate code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
+import rootReducer from "./reducers"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
