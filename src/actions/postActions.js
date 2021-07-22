ed46344804c65cf693755527c1c7928e59ff25b0
prashant dahal <prashantdah41@gmail.com>
2021-07-22 15:54:55 -0400

    Create actions for get request and dispatch accordingly

---
+++ src/actions/postActions.js
@@ -0,0 +1,25 @@
+import { response } from "express";
+import * as actionTypes from "./actionTypes";
+import axios from "../axios"
+
+export const getPosts = () => ({
+    type: actionTypes.GET_POSTS
+})
+
+export const getPostsSuccess = (posts) => ({
+    type: actionTypes.GET_POSTS_SUCCESS,
+    payload: posts
+})
+
+export const getPostsFailure = () => ({
+    type: actionTypes.GET_POSTS_FAILURE   
+})
+
+export const fetchPosts = () => {
+    return async (dispatch) => {
+        dispatch(getPosts())
+        axios.get("posts")
+        .then(response => dispatch(getPostsSuccess(response)))
+        .catch(error => dispatch(getPostsFailure()))
+    }
+}
