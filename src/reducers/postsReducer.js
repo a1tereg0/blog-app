ec692931888bfd25fb8a2ca23d8b987f169e453a
prashant dahal <prashantdah41@gmail.com>
2021-07-22 15:56:13 -0400

    return different states based on the action type

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from "../actions/actionTypes";
+
 const initialState = {
     posts: [],
     loading: false,
@@ -6,6 +8,12 @@ const initialState = {
 
 const postsReducer = (state = initialState, action) => {
     switch(action.type) {
+        case actionTypes.GET_POSTS:
+            return {...state, loading: true};
+        case actionTypes.GET_POSTS_SUCCESS:
+            return {posts: action.payload, loading: false, hasErrors: false}
+            case actionTypes.GET_POSTS_FAILURE:
+                return {...state, loading: false, hasErrors: true}
         default:
             return state;
     }
