a7737bfe5f4711041358e80cd762013d4dc906bd
prashant dahal <prashantdah41@gmail.com>
2021-07-22 15:11:25 -0400

    Create reducers to handle application states

---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+    posts: [],
+    loading: false,
+    hasErrors: false
+};
+
+const postsReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+export default postsReducer;
+
