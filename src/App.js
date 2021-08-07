ea2ba80e59e5c216adbb9102544bc3a8a6b05d91
prashant dahal <prashantdah41@gmail.com>
2021-08-07 19:32:57 -0400

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Posts from "./components/Posts/Posts";
+import FullPost from "./components/FullPost/FullPost";
 
 const App = () => {
-  return <div>Hello, World!</div>
-}
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Posts} />
+        <Route exact path="/:id" component={FullPost} />
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
