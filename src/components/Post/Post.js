eeac84a60480116a06485544da03a8bdc657b345
prashant dahal <prashantdah41@gmail.com>
2021-08-13 11:14:37 -0400

    Remove element

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -4,9 +4,6 @@ import classes from "./Post.module.css";
 const Post = (props) => (
   <article onClick={props.clicked} className={classes.Post}>
     <h1>{props.post.title}</h1>
-    <div className="Info">
-      <div className="Author">Written By: Prashant</div>
-    </div>
   </article>
 );
 
