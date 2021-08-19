9e9ce182dab6066cf86f284a5da71e8a105732ea
prashant dahal <prashantdah41@gmail.com>
2021-08-19 16:03:05 -0400

    Add actions for authentication

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import * as actionTypes from "./actionTypes";
 
 export const startAuth = () => {
@@ -6,22 +7,49 @@ export const startAuth = () => {
   };
 };
 
-export const authSuccess = (auth) => {
+export const authSuccess = (idToken, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    auth: auth,
+    idToken,
+    userId,
   };
 };
 
-export const authFail = (error) => {
+export const authFail = () => {
   return {
     type: actionTypes.AUTH_FAIL,
-    auth: error,
+  };
+};
+
+export const signOut = () => {
+  return {
+    type: actionTypes.AUTH_SIGN_OUT,
+  };
+};
+
+export const authTimeout = (expiresIn) => {
+  return (dispatch) => {
+    setTimeout(() => dispatch(signOut()), +expiresIn * 1000);
   };
 };
 
 export const auth = (email, password) => {
   return (dispatch) => {
     dispatch(startAuth());
+    const data = { email, password, returnSecureToken: true };
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAy72xnK6xSObj6y3i9mM1qFNFJPz4BBKA",
+        data
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(authTimeout(response.data.expiresIn));
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch(authFail());
+      });
   };
 };
