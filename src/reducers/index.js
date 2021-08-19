03f1e5e02976595d50d9937612734e1f83b40f25
prashant dahal <prashantdah41@gmail.com>
2021-08-19 16:06:52 -0400

    Add authReducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
-import postsReducer from './postsReducer';
+import postsReducer from "./postsReducer";
+import authReducer from "./authReducer";
 
 const rootReducer = combineReducers({
-    posts: postsReducer
+  posts: postsReducer,
+  auth: authReducer,
 });
 
 export default rootReducer;
