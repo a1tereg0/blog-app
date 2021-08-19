27921f8c786ac3a6d7c4aa27b1f76b4d2c6f3472
prashant dahal <prashantdah41@gmail.com>
2021-08-19 14:18:18 -0400

    Change baseURL from dummy api to firebase

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com/",
+  baseURL: "https://blog-app-b7ede-default-rtdb.firebaseio.com",
 });
 
 export default instance;
